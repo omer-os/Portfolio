5ba02103e1bc2ee061b2ff422e63b6b24674c644
Nye Evans <nyeevans@outlook.com>
2021-01-01 18:06:19 +0000

    readding files
    github glitched out and thought some files existed when they didnt, solving it by deleting things :)

---
+++ pages/Gallery.js
@@ -0,0 +1,24 @@
+import {slidem, slides} from "GalleryIndex.js";
+
+var currentSlide = 0;
+
+
+function nextSlide(){
+    currentSlide++;
+    updateSlide();
+}
+
+function lastSlide(){
+    currentSlide--;
+    updateSlide();
+}
+
+function updateSlide(){
+    if (currentSlide > slides.length){
+        currentSlide = 0;
+    } else if (currentSlide < 0){
+        currentSlide = slides.length;
+    }
+
+    console.log(slides[currentSlide]);
+}
---
+++ pages/GalleryIndex.js
@@ -0,0 +1,11 @@
+export class slide{
+    constructor(title, caption, image){
+        this.title = title;
+        this.caption = caption;
+        this.image = image;
+    }
+};
+export var slides = [slide(),
+    slide(), 
+    slide()
+]
