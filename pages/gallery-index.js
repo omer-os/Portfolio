2c2290d48d57121d78bbc4dfcfa32475f7365166
Nye Evans <nyeevans@outlook.com>
2021-01-02 19:43:07 +0000

    reuploading files

---
+++ pages/gallery-index.js
@@ -0,0 +1,12 @@
+export class slide{
+    constructor(title, caption, image, link){
+        this.title = title;
+        this.caption = caption;
+        this.image = image;
+        this.link = link;
+    }
+}
+export var slides = [new slide("john", "he is john", "gallery/Cat_tower_defense/projectHeaderImage.png"),
+    new slide("jack", "he is jack", "gallery/Room_builder/projectHeaderImage.png"),
+    new slide("james", "he is james", "gallery/EPQ_project/projectHeaderImage.png")
+];
