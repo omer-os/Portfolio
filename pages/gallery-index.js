d8f1b7904b6f306ae6976feb635b0d6662bb5820
atlastrinity <nyeevans@outlook.com>
2021-01-05 13:47:44 +0000

    adding projects to gallery

--- pages/gallery-index.js
+++ pages/gallery-index.js
@@ -10,20 +10,26 @@ export class slide{
 export var slides = [
     new slide(
         "VR interaction system and physics engine", 
-        "A project done for my EPQ at secondary school, this was my first exploration into physics simulation, virtual reality development and Unreal Engine.<br><br>During this project I created a basic system that allowed the user to grab and throw items with their hands that were tracked with 6 degrees of freedom, this system also had full finger tracking. This was all implemented using Unreal Engines \"Blueprint\" visual programming language. During this I also used C++ within Unreal Engine to try to create a basic physics engine that I could integrate into my interaction system. I managed to finish movement and general force calculations but didnt manage to implement rotation before my EPQ had to be handed in so ended up having to leave the physics system out of the final product and use Unreals \"Chaos\" engine instead.<br><br>This was one of my favorite projects and until my switch to Unity for VR developemnt, became a base for most of my later VR experiments. For a month or so after this, if I ever had an idea for a new weapon I would like to try put in VR I would use this project as my base to build off", 
+        "A project done for my EPQ, during post-16, where I aimed to create a virtual reality environment where the user could grab and interact with objects in a semi-physics based \
+        environment.<br><br>This system was my introduction to Unreal Engine and mainly used its \"Blueprint\" visual language, however I later used C++ to try and add my own physics \
+        to it.", 
         "pages/gallery/EPQ_project/projectHeaderImage.png", 
         "https://www.bbc.co.uk/"
         ),
     new slide(
-        "jack", 
-        "he is jack", 
-        "pages/gallery/Room_builder/projectHeaderImage.png", 
-        "https://twitter.com/"
-        ),
-    new slide(
-        "james", 
-        "he is james", 
+        "Tower defense game", 
+        "A project I created with a friend to help us gain a better understanding of pygame. <br><br> This game was a basic tower defence game but featured a fully scalable and customisable \
+        level system allowing users to add their own levels and towers. One of my earliest projects, this program originally was incredibly inefficient and suffered greatly from feature creep \
+        but was later revisited and improved upon.", 
         "pages/gallery/Cat_tower_defense/projectHeaderImage.png", 
         "https://www.google.com/"
+        ),
+    new slide(
+        "House designer", 
+        "Originally a task given to do in a school lesson with a text interface, this became a small personal project that I added many other features to.<br><br> It included \
+        a graphical interface that allowed the user to draw, move and label rooms as well as set the painting and carpeting price which could later be used to calculate \
+        full decoration prices.", 
+        "pages/gallery/Room_builder/projectHeaderImage.png", 
+        "https://twitter.com/"
         )
 ];  
