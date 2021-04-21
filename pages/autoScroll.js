f4057bb47fea938d92e854f2133704b193125d28
Nye Evans <nyeevans@outlook.com>
2021-04-21 23:31:43 +0100

    updates to cv and aardvark work

--- pages/autoScroll.js
+++ pages/autoScroll.js
@@ -1,4 +1,4 @@
 setTimeout(() => {
     if (window.scrollY < 20)
-        window.scrollBy({top: window.innerHeight, left: 0, behavior: 'smooth'})
-}, 2500); // we scroll the screen down a bit so the user realises theres more 
+        window.scrollBy({top: window.innerHeight - 70, left: 0, behavior: 'smooth'})
+}, 1800); // we scroll the screen down a bit so the user realises theres more 
